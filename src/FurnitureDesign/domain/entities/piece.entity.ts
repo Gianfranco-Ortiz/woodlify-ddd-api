9df9d125ebf0b694b2cc61edf4b2debd5dbb8b7e
Gianfranco-Ortiz <u201916837@upc.edu.pe>
2022-06-05 11:16:11 -0500

    Furniture Design Bounded Context

---
+++ src/FurnitureDesign/domain/entities/piece.entity.ts
@@ -0,0 +1,33 @@
+import { Edge } from "./edge.entity";
+import { Texture } from "./texture.entity";
+import { PieceId } from "../value-objects/piece-id.value"
+
+
+
+export class Piece {
+    private _faces: Edge[];
+    private _textures: Texture[];
+    
+    constructor(
+        private _id: PieceId,
+        private _length: Number,
+        private _height: Number,
+        private _width: Number,
+        private _name: string
+    ) {
+        this._faces = [];
+        this._textures = [];
+    }
+    addFace(face: Edge): void {
+        this._faces.push(face);
+    }
+    getFaces(): Edge[] {
+        return [...this._faces];
+    }
+    addTexture(texture: Texture): void {
+        this._textures.push(texture);
+    }
+    getTextures(): Texture[] {
+        return [...this._textures];
+    }
+}
